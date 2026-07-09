407a106de1b92d3116406c5f3677ab5c3b5d20c5
Thore Strassburg <thore@strassburg.me>
2026-07-09 08:45:09 +0200

    test: remove initial example tests
    These tests were introduced when simply setting up the test frameworks.
    Back then, there was no actual code that could have been tested. We
    wanted it to be available first, so we can practice test-driven
    development. Now that we have actual tests, this is no more of any
    value.

--- package/src/nachricht/zahlungsklage/index.ts
+++ package/src/nachricht/zahlungsklage/index.ts
@@ -1,32 +1,3 @@
 export function zahlungsklage(streitwert: number): string {
   return `<nachricht.klaver.klageverfahren.3500001>${streitwert}</<nachricht.klaver.klageverfahren.3500001>`;
 }
-if (import.meta.vitest) {
-  const { describe, it, expect } = import.meta.vitest;
-
-  describe("Zahlungsklage", async () => {
-    const {
-      assert,
-      property,
-      integer: arbitraryInteger,
-    } = await import("fast-check");
-
-    it("just works fine", () => {
-      const nachricht = zahlungsklage(1);
-
-      expect(nachricht).toEqual(
-        "<nachricht.klaver.klageverfahren.3500001>1</<nachricht.klaver.klageverfahren.3500001>",
-      );
-    });
-
-    it("always includes the Streitwert in the Nachricht", () => {
-      assert(
-        property(arbitraryInteger(), (streitwert) => {
-          const nachricht = zahlungsklage(streitwert);
-
-          expect(nachricht).toContain(streitwert);
-        }),
-      );
-    });
-  });
-}
